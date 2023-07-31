37bc7df6d06c91e38c342348ddd1407cc9ea1363
Jere Nurminen <jnurminen@L1864.local>
2023-07-31 21:40:46 +0300

    Add simple test tile set to try out

---
+++ tiled/foreground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="foreground" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="src/img/foreground.png" trans="ff00ff" width="48" height="48"/>
+</tileset>
