f7c51e6ba36c1aa650db4ef3cc7bfeb1c1e501e1
Jere Nurminen <jnurminen@L1864.local>
2023-08-31 19:10:20 +0300

    Improve test level

--- tiled/foreground.tsx
+++ tiled/foreground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="foreground" tilewidth="16" tileheight="16" tilecount="9" columns="3">
- <image source="src/img/foreground.png" trans="ff00ff" width="48" height="48"/>
+<tileset version="1.10" tiledversion="1.10.0" name="foreground" tilewidth="16" tileheight="16" tilecount="42" columns="6">
+ <image source="../source/images/foreground-table-16-16.png" trans="ff00ff" width="96" height="112"/>
 </tileset>
